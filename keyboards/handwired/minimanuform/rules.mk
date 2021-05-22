3c42071a52cdae00c81196b0fe85d58a9b0cae16
Bernhard Urban-Forster <lewurm@gmail.com>
2021-05-22 21:26:33 +0200

    my keymap

--- keyboards/handwired/minimanuform/rules.mk
+++ keyboards/handwired/minimanuform/rules.mk
@@ -48,10 +48,10 @@ MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
-BOOTLOADER = qmk-dfu
 CONSOLE_ENABLE = yes
 SPLIT_KEYBOARD = yes
-BOOTLOADER = qmk-dfu
+# BOOTLOADER = qmk-dfu
+BOOTLOADER = caterina
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 
