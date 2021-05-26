5cc98ce51837f3dcb642afb8e8fd21ca4f3c09f5
Bernhard Urban-Forster <lewurm@gmail.com>
2021-05-26 12:16:22 +0200

    .

--- keyboards/handwired/minimanuform/keymaps/lewurm/keymap.c
+++ keyboards/handwired/minimanuform/keymaps/lewurm/keymap.c
@@ -13,6 +13,7 @@ enum {
 
 #define _QWERTY 0
 #define _NUMBERS 1
+#define _MISC 2
 
 #define _CODE MO(_COMMAND)
 #define _FSYM LT(_SYMBOLS,KC_F)
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┼           ├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     //└────────┴────────┼────────┼────────┼────────┼────────┤           ├────────┼────────┼────────┼────────┼────────┴────────┘
-                         KC_LEFT, KC_LALT, KC_BSPC, KC_SPC,              KC_ENT,  KC_LCTL, KC_DOWN, KC_UP,
+                         KC_LEFT, KC_LALT, KC_SPC,   KC_BSPC,              KC_ENT,  KC_LCTL, KC_DOWN, KC_UP,
     //                  └────────┴────────┴────────┼────────┤           ├────────┼────────┴────────┴────────┘
                                                      KC_LGUI,           MO(_NUMBERS)
     //                                             └────────┘           └────────┘
@@ -59,9 +60,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┤           ├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LCBR, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_RCBR,             KC_PLUS,  KC_4,    KC_5,   KC_6,   KC_0, KC_MEDIA_PLAY_PAUSE,
     //├────────┼────────┼────────┼────────┼────────┼────────┼           ┌────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_TRNS, KC_TRNS, KC_EQL,  KC_LEFT, KC_RIGHT, KC_TRNS,           KC_MINUS,  KC_1,    KC_2,   KC_3,   KC_MUTE,  RESET,
+        KC_TRNS, KC_TRNS, KC_EQL,  KC_LEFT, KC_RIGHT, KC_TRNS,           KC_MINUS,  KC_1,    KC_2,   KC_3,   KC_MUTE,  KC_TRNS,
     //└────────┴────────┼────────┼────────┼────────┼────────┤           ├────────┼────────┼────────┼────────┼────────┴────────┘
-                         KC_LEFT, KC_LALT, KC_BSPC, KC_SPC,              KC_ENT,  KC_LCTL, KC_VOLD, KC_VOLU,
+                         KC_LEFT, KC_TRNS, KC_BSPC, KC_SPC,              MO(_MISC), KC_TRNS, KC_VOLD, KC_VOLU,
+    //                  └────────┴────────┴────────┼────────┤           ├────────┼────────┴────────┴────────┘
+                                                     KC_LGUI,             KC_TRNS
+    //                                             └────────┘           └────────┘
+    ),
+
+   	[_MISC] = LAYOUT_5x6(
+    //┌────────┬────────┬────────┬────────┬────────┬────────┐           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+        KC_TILD, KC_EXLM,  KC_AT, KC_HASH,  RESET,  KC_PERC,             KC_CIRC,  KC_7,    KC_8,   KC_9,   KC_ASTR, KC_EQL,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤           ├────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_LCBR, KC_DEL, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END,             KC_PLUS,  KC_4,    KC_5,   KC_6,   KC_0, KC_MEDIA_PLAY_PAUSE,
+    //├────────┼────────┼────────┼────────┼────────┼────────┼           ┌────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_TRNS, KC_TRNS, KC_EQL,  KC_LEFT, KC_RIGHT, KC_TRNS,           KC_MINUS,  KC_1,    KC_2,   KC_3,   KC_MUTE,  KC_TRNS,
+    //└────────┴────────┼────────┼────────┼────────┼────────┤           ├────────┼────────┼────────┼────────┼────────┴────────┘
+                         KC_LEFT, KC_LALT, KC_BSPC, KC_SPC,              KC_TRNS,  KC_LCTL, KC_VOLD, KC_VOLU,
     //                  └────────┴────────┴────────┼────────┤           ├────────┼────────┴────────┴────────┘
                                                      KC_LGUI,             KC_TRNS
     //                                             └────────┘           └────────┘
